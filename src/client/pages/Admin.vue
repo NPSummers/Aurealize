fb099b47ffcd071205b590902ee096071ef6bf38
NPSummers <typhfun@outlook.com>
2026-05-18 12:46:52 -0400

    Update Admin.vue

--- src/client/pages/Admin.vue
+++ src/client/pages/Admin.vue
@@ -70,7 +70,7 @@ async function writeNfc(slot: 1 | 2) {
 
   try {
     const writer = new window.NDEFReader();
-    await writer.write(cardPath(slot));
+    await writer.write({ records: [{ recordType: 'url', data: cardPath(slot) }] });
     if (slot === 1) wroteLink1.value = true;
     if (slot === 2) wroteLink2.value = true;
     message.value = `Tag ${slot} was written.`;
